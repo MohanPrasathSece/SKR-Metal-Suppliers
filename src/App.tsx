358515922268bc1ace7b7ab7fe98a6218b133646
MohanPrasathSece <mohanprasath.s2023ece@sece.ac.in>
2026-02-05 16:55:22 +0530

    Add web manifest + logo site icons and enable Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -36,6 +37,7 @@ const App = () => (
             <Route path="/guide/roofing-washer-failure-signs" element={<WasherFailureSignsGuide />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
+          <Analytics />
         </BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>
